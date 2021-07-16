194aa6c237e8b1b4e3068aeb4e3710db5835e177
mi11km <mi11km.basket@gmail.com>
2021-07-17 00:50:26 +0900

    connect mysql

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
+import os
+import mysql.connector as db
+
 
 def main():
-    print("Hello, world!")
+    mysql_connection_config = {
+        'host': os.getenv('MYSQL_HOST', 'db'),
+        'port': os.getenv('MYSQL_PORT', '3306'),
+        'user': os.getenv('MYSQL_USER', 'worker'),
+        'password': os.getenv('MYSQL_PASSWORD', 'dummy'),
+        'database': os.getenv('MYSQL_DATABASE', 'python'),
+    }
+
+    conn = db.connect(**mysql_connection_config)
+    conn.ping(reconnect=True)
+
+    print(conn.is_connected())
+
+    conn.close()
 
 
 if __name__ == '__main__':
