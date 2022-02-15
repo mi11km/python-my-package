a2de6f578cb1e6753de810bdffa9116e02e4a956
mi11km <mi11km.basket@gmail.com>
2022-02-15 20:46:55 +0900

    add

--- src/ai/decision_tree.py
+++ src/ai/decision_tree.py
@@ -11,12 +11,6 @@ class HumanData:
         self.is_buy_wine: bool = buy_wine == "YES"
 
 
-class Node(object):
-    def __init__(self, attribute):
-        self.__attribute = attribute
-        self.__children: List[Node] = []
-
-
 class DecisionTreeQuestion(object):
     def __init__(self):
         self.dataset = [
@@ -39,11 +33,26 @@ class DecisionTreeQuestion(object):
         self.current_information_entropy = self.calculate_information_entropy(self.current_classification)
 
     @staticmethod
+    def split_dataset(dataset: List[HumanData], attribute: str):
+        split_dataset = []
+        if attribute == "年収":
+            split_dataset.append([data for data in dataset if data.is_high_salary])
+            split_dataset.append([data for data in dataset if not data.is_high_salary])
+        if attribute == "性別":
+            split_dataset.append([data for data in dataset if data.is_male])
+            split_dataset.append([data for data in dataset if not data.is_male])
+        if attribute == "旅行":
+            split_dataset.append([data for data in dataset if data.is_like_trip])
+            split_dataset.append([data for data in dataset if not data.is_like_trip])
+        return split_dataset
+
+    @staticmethod
     def calculate_information_entropy(classification_numbers: List[int]) -> float:
         """ 情報エントロピーを計算する関数 """
         total = sum(classification_numbers)
         return -sum(
-            [classification / total * math.log2(classification / total) for classification in classification_numbers])
+            [classification / total * math.log2(classification / total) for classification in classification_numbers if
+             classification != 0])
 
     @staticmethod
     def classify_buy_wine(dataset: List[HumanData]) -> List[int]:
@@ -56,18 +65,45 @@ class DecisionTreeQuestion(object):
             + sum(is_no) / sum(q.current_classification) * q.calculate_information_entropy(is_no)
         return self.current_information_entropy - information_entropy
 
+    def select_attribute(self, dataset: List[HumanData], except_attributes: List[str]):
+        decreasing_information_entropy = {}
+
+        if not ("年収" in except_attributes):
+            is_ok_dataset = q.classify_buy_wine([data for data in dataset if data.is_high_salary])
+            is_no_dataset = self.classify_buy_wine([data for data in dataset if not data.is_high_salary])
+            decreasing_information_entropy["年収"] = self.calculate_decreasing_information_entropy(is_ok_dataset,
+                                                                                                 is_no_dataset)
+
+        if not ("性別" in except_attributes):
+            is_ok_dataset = self.classify_buy_wine([data for data in dataset if data.is_male])
+            is_no_dataset = self.classify_buy_wine([data for data in dataset if not data.is_male])
+            decreasing_information_entropy["性別"] = self.calculate_decreasing_information_entropy(is_ok_dataset,
+                                                                                                 is_no_dataset)
+
+        if not ("旅行" in except_attributes):
+            is_ok_dataset = self.classify_buy_wine([data for data in dataset if data.is_like_trip])
+            is_no_dataset = self.classify_buy_wine([data for data in dataset if not data.is_like_trip])
+            decreasing_information_entropy["旅行"] = self.calculate_decreasing_information_entropy(is_ok_dataset,
+                                                                                                 is_no_dataset)
+
+        biggest_attribute = max(decreasing_information_entropy, key=decreasing_information_entropy.get)
+        return biggest_attribute, decreasing_information_entropy[biggest_attribute]
+
 
 if __name__ == '__main__':
     q = DecisionTreeQuestion()
 
-    is_ok = q.classify_buy_wine([data for data in q.dataset if data.is_high_salary])
-    is_no = q.classify_buy_wine([data for data in q.dataset if not data.is_high_salary])
-    print("情報エントロピーの減少量：", q.calculate_decreasing_information_entropy(is_ok, is_no))
+    selected_attribute = q.select_attribute(q.dataset, [])
+    print(selected_attribute)  # (1)の答え
 
-    is_ok = q.classify_buy_wine([data for data in q.dataset if data.is_male])
-    is_no = q.classify_buy_wine([data for data in q.dataset if not data.is_male])
-    print("情報エントロピーの減少量：", q.calculate_decreasing_information_entropy(is_ok, is_no))
-
-    is_ok = q.classify_buy_wine([data for data in q.dataset if data.is_like_trip])
-    is_no = q.classify_buy_wine([data for data in q.dataset if not data.is_like_trip])
-    print("情報エントロピーの減少量：", q.calculate_decreasing_information_entropy(is_ok, is_no))
+    # (2)を求めるのに参考にしたプログラム
+    datasets = q.split_dataset(q.dataset, selected_attribute[0])
+    print([[data.id for data in dataset] for dataset in datasets])
+    next_datasets = []
+    for dataset in datasets:
+        attribute = q.select_attribute(dataset, [selected_attribute[0]])
+        next_dataset = q.split_dataset(dataset, attribute[0])
+        next_datasets += next_dataset
+        print(attribute)
+        print([[data.id for data in dataset] for dataset in next_dataset])
+    print([[data.id for data in dataset] for dataset in next_datasets])
